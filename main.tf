7a0c2283e8c8939e5a1719f0e8196cb6d251e31d
Shruti Gupta <shrutigupta152005@gmail.com>
2026-01-29 16:01:35 +0530

    Rename EC2 instance to 'project-a-server-test'

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ module "ec2" {
   source        = "git::https://github.com/shrutigupta152005/terraform-modules.git//ec2"
   ami           = "ami-0061e2cfb716149cb"
   instance_type = "t2.micro"
-  name          = "project-a-server"
+  name          = "project-a-server-test"
 }
